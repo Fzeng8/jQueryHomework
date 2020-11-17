aedd513661b122c36f6f6ddf5b184184ceb1aae4
Faith Zeng <faith@Faiths-MacBook-Pro-3.local>
2020-11-17 00:01:14 -0500

    added jquery listeners and functions

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const app = {
-  initialize: function () {
-    // set up listeners here
-  },
+    initialize: function () {
+
+        //click functions
+        $('.logo').dblclick(function () {
+            $('.buisnessDes').toggleClass('appear');
+        });
+        $('.pandoraImg').click(function () {
+            $('.pandoraDes').toggleClass('appear');
+        });
+        $('.averyImg').click(function () {
+            $('.averyDes').toggleClass('appear');
+        });
+
+        //hover functions
+
+        $('.firstImg').hover(function () {
+            $(this).toggleClass('firstTransform');
+        });
+        $('.secondImg').hover(function () {
+            $(this).toggleClass('secondTransform');
+        });
+
+    },
 }
